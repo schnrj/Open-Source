eee40e264a2f6e4657110583d1c4f16c40232c4a
Sachin Raj <120590207+schnrj@users.noreply.github.com>
2024-12-29 16:57:37 +0530

    Time: 8 ms (36.29%) | Memory: 12.7 MB (34.85%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,30 @@
 class Solution {
+
+     int memo[501][501][2]; 
+
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        memset(memo, -1, sizeof(memo)); 
+        return helper(0, piles.size() - 1, piles, 1) >= 0;
+    }
+
+    int helper(int l, int r, vector<int>& piles, int ID) {
+        if (r < l) {
+            return 0;
+        }
+        if (memo[l][r][ID] != -1) {
+            return memo[l][r][ID];
+        }
+
+        int next = 1 - ID; 
+        if (ID == 1) {
+            memo[l][r][ID] = max(piles[l] + helper(l + 1, r, piles, next),
+                                 piles[r] + helper(l, r - 1, piles, next));
+        } else {
+            memo[l][r][ID] = min(-piles[l] + helper(l + 1, r, piles, next),
+                                 -piles[r] + helper(l, r - 1, piles, next));
+        }
+
+        return memo[l][r][ID];
     }
 };
