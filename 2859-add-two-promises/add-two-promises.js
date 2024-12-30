32a09604ce907192597a2587ca3de484acdb85fd
Sachin Raj <120590207+schnrj@users.noreply.github.com>
2024-12-30 19:02:24 +0530

    Time: 55 ms (89.45%) | Memory: 49.7 MB (39.67%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,8 +4,13 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    const [v1,v2]=await Promise.all([promise1,promise2]);
-    return v1+v2;
+    // const [v1,v2]=await Promise.all([promise1,promise2]);
+    // return v1+v2;
+    const [v1]=await Promise.all([promise1]);  
+    
+    
+    const [v2]=await Promise.all([promise2]);
+    return v1+v2;  
 };
 
 /**
