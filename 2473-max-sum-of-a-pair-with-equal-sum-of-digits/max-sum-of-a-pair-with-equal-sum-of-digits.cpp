7f294f294f6ca96d69019b5bb587bc72a89a50ec
Sachin Raj <120590207+schnrj@users.noreply.github.com>
2025-02-12 21:45:08 +0530

    Time: 41 ms (35.08%) | Memory: 63.7 MB (87.26%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int maximumSum(vector<int>& nums) {
-    int res = -1, d_n[82] = {}; 
-    for (int n : nums) {
-        int d = 0;
-        for (int nn = n; nn; nn /= 10)
-            d += nn % 10;
-        if (d_n[d])
-            res = max(res, d_n[d] + n);
-        d_n[d] = max(d_n[d], n);
+        map<int,int>mp;
+        int ans = -1;
+
+        for(auto &e: nums){
+            int curr_sum = 0, num = e;
+            while(num){
+                curr_sum += num%10;
+                num/=10;
+            }
+
+            if(mp.find(curr_sum) != mp.end()){
+            
+                ans = max(ans, e + mp[curr_sum]); 
+               
+                mp[curr_sum] = max(e, mp[curr_sum]);
+            }else mp[curr_sum] = e;
+        }
+        return ans;
     }
-    return res;
-}
 };
