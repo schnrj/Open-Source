6dd6a4b0e2766b5ff71de380e4fe1da544a17699
Sachin Raj <120590207+schnrj@users.noreply.github.com>
2025-05-29 04:30:00 +0530

    Time: 379 ms (30.55%) | Memory: 199.7 MB (35.65%) - LeetSync

--- 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -1,29 +1,56 @@
 class Solution {
-public:
-    vector<vector<int>> buildList(const vector<vector<int>>& edges) {
-        vector<vector<int>> adj(edges.size() + 1);
-        for (auto &e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+    #define pii pair<int,int>
+    
+    int BFS(int start,vector<vector<int>>& adj,int k){
+        queue<pii> q;
+        q.push(make_pair(start,-1));
+        int count = 0;
+        
+        while(!q.empty() and k>=0){
+            int size=q.size();
+            count += size;
+            for(int i=0;i<size;++i){
+                pii curr = q.front();
+                q.pop();
+                
+                int u = curr.first;
+                int parent = curr.second;
+                for(int v: adj[u]){
+                    if(v!=parent){
+                        q.push(make_pair(v,u));
+                    }
+                }
+            }
+            k--;
         }
-        return adj;
+        return count;
     }
-    
-    int dfs(const vector<vector<int>>& adj, int u, int p, int k) {
-        if (k < 0) return 0;
-        int cnt = 1;
-        for (int v : adj[u])
-            if (v != p) cnt += dfs(adj, v, u, k-1);
-        return cnt;
-    }
-    
+public:
     vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
-        auto adj1 = buildList(edges1), adj2 = buildList(edges2);
-        int n = adj1.size(), m = adj2.size(), maxiB = 0;
-        vector<int> res(n);
-
-        for (int i = 0; i < m; i++) maxiB = max(maxiB, dfs(adj2, i, -1, k - 1));
-        for (int i = 0; i < n; i++) res[i] = dfs(adj1, i, -1, k) + maxiB;
+        int m = edges1.size()+1;
+        int n = edges2.size()+1;
+        vector<vector<int>> adj1(m),adj2(n);
+        for(auto& edge: edges1){
+            adj1[edge[0]].push_back(edge[1]);
+            adj1[edge[1]].push_back(edge[0]);
+        }
+        for(auto& edge: edges2){
+            adj2[edge[0]].push_back(edge[1]);
+            adj2[edge[1]].push_back(edge[0]);
+        }
+     
+        int best=0;
+        for(int i=0;i<n;++i){
+            int connections = BFS(i,adj2,k-1);
+            best = max(best,connections);
+        }
+        
+       
+        vector<int> res(m);
+        for(int i=0;i<m;++i){
+            int connections = BFS(i,adj1,k);
+            res[i] = connections + best;
+        }
         return res;
     }
 };
