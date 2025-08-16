147a4b699ff6626f80a5336d9ef22e24452ec597
Sachin Raj <120590207+schnrj@users.noreply.github.com>
2025-08-16 11:51:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (18.44%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==1) return true;
-        long long  x=1;
-        while(x<n){
-            x=x*4;
+        // if(n==1) return true;
+        // long long  x=1;
+        // while(x<n){
+        //     x=x*4;
 
+        // }
+        // if(x==(long long)n) return true;
+        // else return false;
+        double res=log(n)/log(4);
+        if(fabs(res-round(res))<1e-9){
+            return true;
         }
-        if(x==(long long)n) return true;
         else return false;
     }
 };
