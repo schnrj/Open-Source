924d1ae248e21ef7e416571256b09747872d8741
Sachin Raj <120590207+schnrj@users.noreply.github.com>
2025-08-21 07:19:15 +0530

    Time: 544 ms (5.04%) | Memory: 61.2 MB (6.58%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        int ans=0;
-        int x=0;
-        int j=0;
-        for(int i=0;i<nums.size();i++){
-            while((x&nums[i])!=0){
-                x=x^nums[j];
-                j++;
+        int ans = 1;
+        int i = 0;
+        int n = nums.size();
+        unordered_map<int, int> bit;
+
+        for (int j = 0; j < n; j++) {
+            bool f = false;
+            for (int k = 0; k < 32; k++) {
+                if (nums[j] & (1 << k)) {
+                    bit[k]++;
+                }
+                if (bit[k] > 1) {
+                    f = true;
+                }
             }
-            x=x|nums[i];
-            ans=max(ans,i-j+1);
+
+            while (f) {
+                for (int k = 0; k < 32; k++) {
+                    if (nums[i] & (1 << k)) {
+                        bit[k]--;
+                    }
+                }
+                i++;
+                f = false;
+                for (int k = 0; k < 32; k++) {
+                    if (bit[k] > 1) {
+                        f = true;
+                        break;
+                    }
+                }
+            }
+
+            ans = max(ans, j - i + 1);
         }
         return ans;
     }
